cc7d6b8dbcc495db57a92f390e24f6a9fe99eee2
Florentina <florentinaantigha27@gmail.com>
2023-11-27 16:25:29 +0100

    Designed and Implemented Animations for About Page

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// Animation for the typing feature
 let type = new Typed("#changing-text", {
   strings: ["Front-end Developer", "DevOps Engineer", "Coding Intructor"],
   typeSpeed: 60,
   backSpeed: 50,
   loop: true,
 });
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab")
+}
